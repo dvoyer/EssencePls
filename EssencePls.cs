2e7314371588259557f42e3984ed473eefbe1778
Diana Voyer <7999399+dvoyer@users.noreply.github.com>
2021-05-29 19:00:56 -0400

    fixed essence not appearing bug, minor reformatting

--- EssencePls.cs
+++ EssencePls.cs
@@ -1,47 +1,61 @@
 ﻿using System;
-using System.Collections;
-using System.Text.RegularExpressions;
-using System.Linq;
-using GlobalEnums;
 using Modding;
-using HutongGames.PlayMaker;
 using UnityEngine;
 
 namespace EssencePls
 {
 	public class deathEffects : EnemyDeathEffects
-    {
-		public GameObject essencePrefab
-        {
-			get { return dreamEssenceCorpseGetPrefab; }
-			set { }
-        }
+	{
+		public GameObject essencePrefab;
     }
 
     public class EssencePls : Mod
     {
-        public override string GetVersion() => "1.0";
+ 		public GameObject _essencePrefab;
+        public deathEffects deathEffects;
+        public override string GetVersion() => "1.0.0";
         public override void Initialize()
         {
             Log("EssencePls v." + GetVersion());
-
-            On.EnemyDeathEffects.EmitEssence += alwaysYes;
+			getPrefab();
+			On.EnemyDeathEffects.EmitEssence += alwaysYes;
         }
+		public int getPrefab()
+		{
+			// this is still very bad but it will work
+			Resources.LoadAll<GameObject>("");
+			foreach (GameObject i in Resources.FindObjectsOfTypeAll<GameObject>())
+			{
+				if (i.name == "dream_essence_corpse_get")
+				{
+					_essencePrefab = i;
+					return 0;
+				}
+				//Log(i.name);
+			}
+			return -1;
+		}
 
-        public void alwaysYes(On.EnemyDeathEffects.orig_EmitEssence orig, EnemyDeathEffects self)
+		public void alwaysYes(On.EnemyDeathEffects.orig_EmitEssence orig, EnemyDeathEffects self)
         {
 			PlayerData playerData = GameManager.instance.playerData;
 			if (!playerData.GetBool("hasDreamNail"))
 			{
 				return;
 			}
-			//deathEffects e = (deathEffects)self;
-			//e.essencePrefab.Spawn(self.transform.position + self.effectOrigin);
-			PlayerData playerData2 = playerData;
-			playerData2.SetInt("dreamOrbs", playerData2.GetInt("dreamOrbs") + 1);
-			PlayerData playerData3 = playerData;
-			playerData3.SetInt("dreamOrbsSpent", playerData3.GetInt("dreamOrbsSpent") - 1);
-			EventRegister.SendEvent("DREAM ORB COLLECT");
+			if (playerData.dreamOrbs < 907 && playerData.dreamOrbs > 1800)
+            {
+				orig(self);
+            }
+			else
+            {
+				_essencePrefab.Spawn(self.transform.position + self.effectOrigin);
+				PlayerData playerData2 = playerData;
+				playerData2.SetInt("dreamOrbs", playerData2.GetInt("dreamOrbs") + 1);
+				PlayerData playerData3 = playerData;
+				playerData3.SetInt("dreamOrbsSpent", playerData3.GetInt("dreamOrbsSpent") - 1);
+				EventRegister.SendEvent("DREAM ORB COLLECT");
+            }
 		}
 	}
 }
